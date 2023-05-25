35cb4cb0281bb67fade1a4a9b02502c24bce7d43
Verenich Tatiana <verenik91@gmail.com>
2023-05-26 00:18:54 +0300

    Добавлен набросок задачи

---
+++ HomeWork2.py
@@ -0,0 +1,24 @@
+# Задача 10:
+# На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом. 
+# Определите минимальное число монеток, которые нужно перевернуть, 
+# чтобы все монетки были повернуты вверх одной и той же стороной. 
+# Выведите минимальное количество монет, которые нужно перевернуть
+
+while True:
+    n = input('Введите количество монет: ')
+    if n.isdigit():
+        n = int(n)
+        i = 0
+        count_one = 0
+        count_null = 0
+        import random
+        random_array = [random.randint(0, 1) for i in range(n)]
+        print(random_array)
+        for i in range(n):
+            if random_array[i] == 1:
+                count_one += 1
+            else: count_null += 1
+        if count_one > count_null:
+            print(count_one)
+            count_null = count_one
+            print(random_array)
